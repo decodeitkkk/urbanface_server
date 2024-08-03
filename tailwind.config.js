8817ce7526994317952c7fb355b4d091b6e53c2d
decodeitkkk <kkailash0086@gmail.com>
2024-08-03 12:53:05 +0530

    playfair-display restored

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,12 @@ module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
         fontFamily: {
-            AbrilFatface: ["Abril-Fatface"],
-            Cormorant: ["Cormorant"],
-            PlayfairDisplay: ["Playfair-Display"],
-            Raleway: ["Raleway"],
-            MontaguSlab: ["Montagu-Slab"],
             FigtreeMedium: ["Figtree-Medium"],
-            FigtreeBold: ["Figtree-Bold"],
+            PlayfairDisplay: ["Playfair-Display"],
             LibreBaskervilleRegular: ["LibreBaskerville-Regular"],
-            LibreBaskervilleBold: ["LibreBaskerville-Bold"],
             SourceSans3Regular: ["SourceSans3-Regular"],
         },
         extend: {},
     },
     plugins: [],
-}; 
+};
